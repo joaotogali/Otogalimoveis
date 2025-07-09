537360497daf98dc0e51a28e52731b887f377cca
João Gabriel Mattos Otogali <jao.otogali@gmail.com>
2025-07-09 15:52:21 -0300

    Add domain entities. #OTI-3 [Task]

---
+++ Otogalimoveis.Domain/Model/Aluguel.cs
@@ -0,0 +1,13 @@
+﻿namespace Otogalimoveis.Domain.Model
+{
+    public class Aluguel
+    {
+        public int Id { get; set; }
+        public Imovel Imovel { get; set; }
+        public int ImovelId { get; set; }
+        public Locatario Locatario { get; set; }
+        public int LocatarioId { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime? DataFim { get; set; }
+    }
+}
